7849a5ef334a5bb7ae18fef26bd2f0219143e124
Mohak Agarwal <155367989+codewithMohak@users.noreply.github.com>
2025-02-24 15:18:10 +0530

    Delete Backend/student-dashboard-backend directory

--- Backend/student-dashboard-backend/routes/StudentRoutes.js
+++
@@ -1,8 +0,0 @@
-const express=require("express");
-const{getStudent,updateStudent}=require("../controllers/studentController");
-const router=express.Router();
-
-router.get("/:id",getStudent);
-router.put("/:id", updateStudent);
-
-module.exports=router;
